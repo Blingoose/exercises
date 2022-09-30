6ab1d62f2537c02f8b99d4d9c910f9372184d1a6
Andre Katov <andy.katov@icloud.com>
2022-09-30 19:44:29 +0300

    minor changes to assignment 11.2

--- javaScript/11.2/forEachAndMap.js
+++ javaScript/11.2/forEachAndMap.js
@@ -1,13 +1,13 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 const array2 = ["This", "is", 3, 4, "a", 6, ["Yo", 2], 8, "Good", "3", "Book"];
 const string =
-  "This is a very irritating assignment to do, but, I'll do it anyway.";
+  "This is a very irritating assignment to do, but, I'll do it anywayz.";
 
-//! -------------Reusable-mapStuff-Function------------------
-const mapStuff = (placeHolder, callback) => {
-  placeHolder.map(callback);
+//! -------------Reusable-doStuff-Function------------------
+const doStuff = (placeHolder, callback) => {
+  placeHolder.forEach(callback);
 };
-// ----------------------------------------------------
+// ------------------------------------------------------------
 
 //! ---------------doubleValues------------------------
 const doubleArr = [];
@@ -16,7 +16,7 @@ const doubleValues = (e) => {
   return doubleArr.push(e * 2);
 };
 
-mapStuff(array, doubleValues);
+doStuff(array, doubleValues);
 console.log(doubleArr);
 // ---------------------------------------
 
@@ -29,7 +29,7 @@ const OnlyEvenValues = (e) => {
   }
 };
 
-mapStuff(array, OnlyEvenValues);
+doStuff(array, OnlyEvenValues);
 console.log(evenArr);
 // ---------------------------------------
 
@@ -41,7 +41,7 @@ const showFirstAndLast = (e, i, arr) => {
     firstLastArr.push(e);
   }
 };
-mapStuff(array2, showFirstAndLast);
+doStuff(array2, showFirstAndLast);
 console.log(firstLastArr);
 
 //! -----------------------vowelCount------------------------------
@@ -49,30 +49,73 @@ const obj = {};
 
 const vowelCount = (e) => {
   const vowel = "aeuoi";
-  let toLower = e.toLowerCase();
-  if (
-    e !== " " &&
-    e !== "'" &&
-    e !== "," &&
-    e !== "." &&
-    vowel.indexOf(toLower) > -1
-  ) {
+  const toLower = e.toLowerCase();
+  if (vowel.indexOf(toLower) > -1) {
     if (!obj[toLower]) {
       obj[toLower] = 1;
     } else obj[toLower]++;
   }
 };
 
-mapStuff(string.split(""), vowelCount);
+doStuff(string.split(""), vowelCount);
 console.log(obj);
 
 //! -------------------------capitalized-------------------------
 const capitalizedStr = [];
 
 const capitalized = (e) => {
-  let counter = 0;
   capitalizedStr.push(e[0].toUpperCase() + e.substr(1));
-  capitalizedStr.join(" ");
 };
-mapStuff(string.split(" "), capitalized);
+doStuff(string.split(" "), capitalized);
 console.log(capitalizedStr.join(" "));
+
+//! ----------------------------shiftLetters------------------------------
+const shiftedStr = [];
+
+const shiftLetters = (word) => {
+  shiftedStr.push(
+    word
+      .split("")
+      .map((char) => {
+        if (char === "a") {
+          return String.fromCharCode(char.charCodeAt() + 25);
+        }
+        return String.fromCharCode(char.charCodeAt() - 1);
+      })
+      .join("")
+  );
+};
+
+doStuff(string.split(" "), shiftLetters);
+console.log(shiftedStr.join(" "));
+
+//**************************************** */ Internet Solution
+// strr = "abcd efgh";
+// const shiftLetters = (string) => {
+//   return string
+//     .split(" ")
+//     .map((word) => {
+//       return word
+//         .split("")
+//         .map((letter) => {
+//           return String.fromCharCode(letter.charCodeAt() + 1);
+//         })
+//         .join("");
+//     })
+//     .join(" ");
+// };
+
+// console.log(shiftLetters(string));
+
+// --------------------------------------------------------------
+
+//! ------------------------------swapCase---------------------------
+const swapArr = [];
+const swapCase = (e, i) => {
+  if (i % 2 === 0) {
+    return swapArr.push(e[0].toUpperCase() + e.substr(1));
+  }
+  return swapArr.push(e[0].toLowerCase() + e.substr(1));
+};
+doStuff(string.split(" "), swapCase);
+console.log(swapArr.join(" "));
