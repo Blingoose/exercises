3a508822b2e9c83b38e61607b9b0800c68f58cbe
Andre Katov <andy.katov@icloud.com>
2022-10-12 02:57:44 +0300

    done

--- javaScript/19.3/app.js
+++ javaScript/19.3/app.js
@@ -1,3 +1,28 @@
-const input = document.querySelectorAll("input");
-arr = [...input];
-console.log(arr);
+const form = document.querySelector("form");
+const inputs = form.querySelectorAll("input");
+
+//set maxlength=1 for each input
+inputs.forEach((input) => (input.maxLength = 1));
+
+function handleInput(e) {
+  //check for data that was inputted and if there is a next input,
+  // focus it.
+  const input = e.target;
+  if (input.nextElementSibling && input.value) {
+    input.nextElementSibling.focus();
+  }
+}
+
+function handlePaste(e) {
+  const paste = e.clipboardData.getData("text");
+  //   loop over each input, and populate with index of that string.
+  inputs.forEach((input, i) => {
+    input.value = paste[i] || "";
+  });
+}
+
+inputs[0].addEventListener("paste", handlePaste);
+
+form.addEventListener("input", handleInput);
+
+123456;
