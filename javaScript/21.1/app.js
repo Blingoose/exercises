0d712f60bcba8cb77bb83d6ea0cabd9afdf6f40f
Andre Katov <andy.katov@icloud.com>
2022-10-13 17:07:56 +0300

    finished

--- javaScript/21.1/app.js
+++ javaScript/21.1/app.js
@@ -1,38 +1,53 @@
+let count = 0;
 const button = document.querySelector(".btn");
 const input = document.querySelector(".age");
 const messageDiv = document.querySelector(".message-holder");
 const messageP = document.createElement("p");
 
+//function to setTimeOut
+function timeOut(func, t) {
+  setTimeout(() => {
+    func();
+  }, t);
+}
+
+//clear poping messages
 function clearMessage() {
   messageP.remove();
+  count = 0;
+}
+
+//handle error message when repeated clicks happen
+function clearRepeated() {
+  if (count === 1) {
+    timeOut(clearMessage, 3000);
+    console.log("no");
+  }
 }
 
 input.addEventListener("input", function () {
-  if (input.value.includes("0")) {
-    messageP.remove();
-  } else if (!Number(this.value) && input.value.length > 0) {
+  if (
+    !Number(this.value) &&
+    input.value.length > 0 &&
+    !input.value.includes("0")
+  ) {
+    count++;
     messageP.innerText = "Numbers only!";
     messageDiv.append(messageP);
-    setTimeout(clearMessage, 2000);
+    clearRepeated();
   }
   input.value = input.value.replace(/\D+/g, "");
 });
 
-// input.addEventListener("keyup", (e) => {
-//   let x = event;
-//   e.preventDefault();
-//   if (KeyboardEvent === "Enter") {
-//     console.log("Enter Is Pressed");
-//     button.click();
-//   }
-// });
-
 button.addEventListener("click", function () {
+  count++;
   if (+input.value >= 18) {
     messageP.innerText = "You can drink!";
     messageDiv.append(messageP);
-  } else {
+    clearRepeated();
+  } else if (input.value !== "") {
     messageP.innerText = "You're too young.";
     messageDiv.append(messageP);
+    clearRepeated();
   }
 });
