55f83f79305fc5b667e6593f114cad6c364193fb
Andre Katov <andy.katov@icloud.com>
2022-10-27 09:01:23 +0300

    finished

--- javaScript/30.1/script.js
+++ javaScript/30.1/script.js
@@ -44,41 +44,45 @@ function checkUserExist(userData) {
     rememberUser(userData);
   } else console.log("Not Esixt");
 }
-// const card = document.querySelector(".card");
-// const img = document.querySelector(".img");
-// const userName = document.querySelector(".user-name");
-// const repos = document.querySelector(".repos");
 
 function rememberUser(user) {
-  userArr.push(user.login);
   userObj = {
+    userLogin: user.login,
     img: user.avatar_url,
     name: user.name,
     link: user.html_url,
     repos: user.public_repos,
   };
+  userArr.push(userObj.userLogin);
+  input.value = "";
   createCard(userObj);
 }
 
-function createCard() {
-  let card = document.createElement("div");
+function createCard(info) {
+  let userLogin = document.createElement("div");
+  let card = document.createElement("a");
   let img = document.createElement("img");
   let userName = document.createElement("div");
   let repos = document.createElement("div");
   card.classList.add("card");
   img.classList.add("img");
   userName.classList.add("user-name");
+  userLogin.classList.add("user-login");
   repos.classList.add("repos");
 
-  img.src = `${userObj.img}`;
-  userName.textContent = userObj.name;
-  repos.textContent = userObj.repos;
+  img.src = `${info.img}`;
+  if (info.name === null) {
+    userName.textContent = "No Username";
+  } else {
+    userName.textContent = `Username: ${info.name}`;
+  }
+  repos.textContent = `Public Repos: ${info.repos}`;
+  card.href = info.link;
+  userLogin.textContent = info.userLogin;
+  card.prepend(userLogin);
   card.appendChild(img);
   card.appendChild(userName);
   card.appendChild(repos);
   cardContainer.appendChild(card);
-  card.addEventListener("click", () => {
-    window.open(`${userObj.link}`, "_blank");
-  });
 }
 start();
