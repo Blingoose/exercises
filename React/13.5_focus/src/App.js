073d17caf340515ca565f08ce97430cd36915247
Andre Katov <andy.katov@icloud.com>
2022-11-14 01:17:40 +0200

    finished assignment

--- React/13.5_focus/src/App.js
+++ React/13.5_focus/src/App.js
@@ -1,8 +1,23 @@
-import React, { useState, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
+  return (
+    <div className="App">
+      <form>
+        <input
+          type={"text"}
+          ref={inputRef}
+          placeholder="This one has focus"
+        ></input>
+        <input type={"text"} placeholder="No focus for that one"></input>
+      </form>
+    </div>
+  );
+};
 export default App;
