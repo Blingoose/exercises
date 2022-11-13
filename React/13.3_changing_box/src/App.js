5fed8e5b44e0430cacf11a0c6df32f7d73f63f00
Andre Katov <andy.katov@icloud.com>
2022-11-14 01:00:10 +0200

    added finished assignment

--- React/13.3_changing_box/src/App.js
+++ React/13.3_changing_box/src/App.js
@@ -1,7 +1,36 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState(0);
+  const colors = [
+    "blue",
+    "green",
+    "red",
+    "purple",
+    "grey",
+    "pink",
+    "orange",
+    "violet",
+  ];
+  useEffect(() => {
+    console.log(count);
+    const timeOut = setTimeout(() => {
+      setCount((prev) => prev + 1);
+    }, 500);
+    if (count === 5) {
+      clearInterval(timeOut);
+    }
+    return () => {
+      clearTimeout(timeOut);
+    };
+  });
+  return (
+    <div
+      className={count < 5 ? "box" : `box ${"circle"}`}
+      style={{ backgroundColor: colors[count] }}
+    ></div>
+  );
 }
 
 export default App;
