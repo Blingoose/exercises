ab99c910ce38486031827567b6635dced8bd81ba
Andre Katov <andy.katov@icloud.com>
2022-11-14 02:23:51 +0200

    finished assignment

--- React/14.1_chuck_norris/src/App.js
+++ React/14.1_chuck_norris/src/App.js
@@ -1,7 +1,56 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+
+// https://api.chucknorris.io/
 
 function App() {
-  return <div className="App"></div>;
+  const [joke, setJoke] = useState("");
+  const [categories, setCategories] = useState([]);
+
+  async function generateJoke() {
+    const { data } = await axios.get("https://api.chucknorris.io/jokes/random");
+    // console.log(data);
+    setJoke(data.value);
+  }
+
+  useEffect(() => {
+    async function getCategories() {
+      const { data } = await axios.get(
+        "https://api.chucknorris.io/jokes/categories"
+      );
+      console.log(data);
+      setCategories(data);
+    }
+    getCategories();
+  }, []);
+
+  async function getJokeByCategory(e) {
+    const category = e.currentTarget.getAttribute("category");
+    const { data } = await axios.get(
+      `https://api.chucknorris.io/jokes/random?category=${category}`
+    );
+
+    setJoke(data.value);
+  }
+
+  return (
+    <div className="App">
+      <ul>
+        {categories.map((category, i) => (
+          <button
+            onClick={getJokeByCategory}
+            key={i + category}
+            category={category}
+          >
+            {category}
+          </button>
+        ))}
+      </ul>
+      <p>{joke}</p>
+      <button onClick={generateJoke}>Get Joke</button>
+    </div>
+  );
 }
 
 export default App;
