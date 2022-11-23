0eaeefbaa2c0df45aa64e5ac146cd66dbd3a170c
Andre Katov <andy.katov@icloud.com>
2022-11-23 12:03:33 +0200

    finished assignment

---
+++ React/21.1_box_animation/src/App.js
@@ -0,0 +1,60 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.abc = 0;
+    this.boxes = [
+      { width: 100, height: 100, color: "black", left: "-200px" },
+      { width: 200, height: 200, color: "green", left: "-300px" },
+      { width: 400, height: 400, color: "blue", left: "-400px" },
+    ];
+
+    this.state = {
+      time: 1000,
+      count: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.abc = setInterval(() => {
+      this.setState((prev) => {
+        return { count: prev.count + 1 };
+      });
+      console.log(this.state.count);
+    }, this.state.time);
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.count === 5 && prevState.count !== this.state.count) {
+      console.log("Hey!!!");
+      clearInterval(this.abc);
+      this.setState((prev) => {
+        return { time: (prev.time = false) };
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.boxes.map((_, i) => (
+          <div
+            key={i}
+            style={{
+              width: this.boxes[i].width,
+              height: this.boxes[i].height,
+              backgroundColor: this.boxes[i].color,
+              position: "relative",
+              left: this.boxes[i].left,
+            }}
+            className={this.state.count && this.state.time ? `box1` : ""}
+          ></div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
